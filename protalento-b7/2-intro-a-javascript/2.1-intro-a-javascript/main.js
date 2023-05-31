6d02efdb92df1a0feef8f5c291527492aeec9b78
Stiven Trujillo <strujillodev@gmail.com>
2023-05-30 23:50:18 -0500

    format main js in intro to Js

--- protalento-b7/2-intro-a-javascript/2.1-intro-a-javascript/main.js
+++ protalento-b7/2-intro-a-javascript/2.1-intro-a-javascript/main.js
@@ -1,13 +1,13 @@
 /* las salidas (para ver información) */
-console.log('hola soy el segundo script', 'hola 2');
-document.write('hola mundo');
-alert('hola soy una ventana emergente simple');
+console.log("hola soy el segundo script", "hola 2");
+document.write("hola mundo");
+alert("hola soy una ventana emergente simple");
 
 /* las entradas (para recibir informacion) */
-var edad = prompt('cual es tu edad');
+var edad = prompt("cual es tu edad");
 // edad = parseInt(edad) + 2;
 
-document.write('<br>tienes ', edad, ' anios')
+document.write("<br>tienes ", edad, " anios");
 
 /* Comentarios */
 // utilizando solo los // comentarios de una sola linea
@@ -18,29 +18,29 @@ linea 2
 
 /* Variables */
 // declaracion - asignacion
-var apellido = 'Hernandez sadfdsafdsafsad';
+var apellido = "Hernandez sadfdsafdsafsad";
 
-console.log('Hernandez sadfdsafdsafsad')
-console.log('Hernandez sadfdsafdsafsad')
-console.log('Hernandez sadfdsafdsafsad')
-console.log('Hernandez sadfdsafdsafsad')
-console.log('Hernandez sadfdsafdsafsad')
+console.log("Hernandez sadfdsafdsafsad");
+console.log("Hernandez sadfdsafdsafsad");
+console.log("Hernandez sadfdsafdsafsad");
+console.log("Hernandez sadfdsafdsafsad");
+console.log("Hernandez sadfdsafdsafsad");
 
-console.log(apellido)
-console.log(apellido)
-console.log(apellido)
-console.log(apellido)
-console.log(apellido)
-console.log(apellido)
+console.log(apellido);
+console.log(apellido);
+console.log(apellido);
+console.log(apellido);
+console.log(apellido);
+console.log(apellido);
 
 // existen 3 formas de declarar variables en javascript
-var nombre = 'stiven'; // var ya no se utiliza ni por el berraco xD
-let mascota = 'michi';
-const ciudad = 'cali';
+var nombre = "stiven"; // var ya no se utiliza ni por el berraco xD
+let mascota = "michi";
+const ciudad = "cali";
 
 // pueden cambiar, varian
-nombre = 'Camilo';
-mascota = 'mordelon';
+nombre = "Camilo";
+mascota = "mordelon";
 //ciudad = 'bogota'; // es un error!!
 
 /* Nombramiento de variables */
@@ -60,17 +60,17 @@ let typeUserMusic;
 let userId;
 
 // Alvaro nos dijo una vez "soy un buen alumno"
-let message = 'Alvaro nos dijo una vez "soy un buen alumno"'
+let message = 'Alvaro nos dijo una vez "soy un buen alumno"';
 
 /* Tipos de Datos */
 /* Numeros */
-let edad2 = 'asdfasdf'
+let edad2 = "asdfasdf";
 
 edad2 = parseInt(edad2);
 // NaN es un numero! esta en el tipo de numeros
 
 console.log("edad: ", edad2);
-console.log(typeof edad2)
+console.log(typeof edad2);
 
 /* Booleanos */
 let areFull = true; // nombramiento de variables booleanas
@@ -91,17 +91,17 @@ paperRoll = undefined; // directamente definirle el valor de indefinido
 /* Objetos */
 let person = {
   age: 30,
-  name: 'Stiven',
+  name: "Stiven",
   direction: {
-    country: 'colombia',
-    city: 'cali',
-    direction: 'CR 89 - 132'
+    country: "colombia",
+    city: "cali",
+    direction: "CR 89 - 132",
   },
-}
+};
 
 /* Operaciones Matematicas */
 let result = 5 + 10;
 let result2 = 5 - 10;
 let result3 = 5 * 10;
 
-console.log(result) // output: 15
+console.log(result); // output: 15
