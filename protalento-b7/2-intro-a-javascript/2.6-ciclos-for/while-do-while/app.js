42c870065553a10eaad44e0623ba954d14e10210
Yaxche <yaxchemanrique@gmail.com>
2023-06-07 19:34:32 -0700

    feat: Se agrega codigo de la clase de ciclos: while, do-while y for

---
+++ protalento-b7/2-intro-a-javascript/2.6-ciclos-for/while-do-while/app.js
@@ -0,0 +1,47 @@
+// let dato = prompt('Ingresa un numero');
+
+// debugger
+
+// while ( dato != 'esc') {
+//     console.log(dato);
+//     dato = prompt('Ingresa un dato');
+// }
+
+// let iterador = 1;
+// debugger
+
+// while ( iterador <= 10) {
+//     console.log(iterador + ' x 12 = ' + iterador*12);
+//     iterador = iterador + 1; //-> iterador++
+// }
+
+//iteracion 1:  1 x 12 = 12
+// 2 x 12 = 24
+// 3 x 12 = 36...
+
+// let dato;
+// do {
+//     dato = prompt('Ingresa un numero del 1 al 10');
+// } while (dato > 10 || dato < 1);
+
+let pokemons = ['pikachu', 'balbasaur', 'dito', 'eve', 'charmander', 'snorlax'];
+
+console.log(pokemons[0])
+console.log(pokemons[1])
+console.log(pokemons[2])
+console.log(pokemons[3])
+console.log(pokemons[4])
+console.log(pokemons[5])
+console.log(pokemons[6])
+
+//   for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+
+// function printArray(array) {
+//   for (let i = 0; i < array.length; i++) {
+//   console.log(array[i]);
+// }
+// }
+
+// printArray(pokemons)
