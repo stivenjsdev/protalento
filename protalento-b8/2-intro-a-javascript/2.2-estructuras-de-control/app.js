09bdc76d295cea1b2402d19592a64ddb6bf53aec
Stiven Trujillo <strujillodev@gmail.com>
2023-06-14 16:16:18 -0500

    feat: modify class example

--- protalento-b8/2-intro-a-javascript/2.2-estructuras-de-control/app.js
+++ protalento-b8/2-intro-a-javascript/2.2-estructuras-de-control/app.js
@@ -1,13 +1,17 @@
+// convertir string a numero: parseInt() - parseFloat() - Number()
 const age = Number(prompt("Cual es tu edad?")); // si el usuario no escribe un numero, age sería NaN
 const residence = prompt("Resides en un municipio fronterizo del pais?");
+console.log({ age });
 
-const isAdult = age >= 18; // Nan >= 18 es NaN, por lo que isAdult seria false
-const isLocateNorthFrontier = residence.toUpperCase() === "SI"; // toUpperCase() - toLowerCase()
+// > = : >=
+const isAdult = age >= 18; // Nan >= 18 es NaN, por lo que isAdult seria false porque NaN es falsy
+const isLocateNorthFrontier = residence?.toLowerCase() === "si"; // toUpperCase() - toLowerCase()
 
-// > =
+// Operador logico And (&&)
 if (isAdult && isLocateNorthFrontier) {
-  document.write("tranquilo te puedes vacunar");
-  console.log("tranquilo te puedes vacunar");
+  const name = prompt('cual es tu nombre?');
+  document.write("Te vamos a vacunar asi que no corras!");
+  console.log("Te vamos a vacunar asi que no corras!");
 } else {
   document.write("lo siento, no te puedes vacunar, largo!");
   console.log("lo siento, no te puedes vacunar, largo!");
