6d61a0a44d6ae6df07a4a81a873215a488230f85
Yaxche <yaxchemanrique@gmail.com>
2023-06-15 19:53:11 -0700

    feat: codigo de clase de Ciclos For

--- protalento-b8/2-intro-a-javascript/2.6-ciclos-for/for/app.js
+++ protalento-b8/2-intro-a-javascript/2.6-ciclos-for/for/app.js
@@ -1,12 +1,72 @@
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${i} x 12 = ${i * 12}`)
+// }
+
+// let pokemons = ['pikachu', 'snorlax', 'eve', 'charmander', 'mew', 'mewto', '', ''];
+
+// console.log(pokemons[0]);
+// console.log(pokemons[1]);
+// console.log(pokemons[2]);
+// console.log(pokemons[3]);
+
+// console.log(pokemons.length)
+
+// for(let i = 0; i <= 5; i++) {
+//   console.log(pokemons[i]);
+// }
+
+// for(let i = 0; i < pokemons.length; i++) {
+//   console.log(pokemons[i]);
+// }
+
+
+// Continue
 let array = [0, 1, 2, 3, 4, 5, 6];
+
+// que no imprima el num 3
+// for (let i = 0; i < array.length; i++){
+//   if(array[i] === 3) {
+//     continue
+//   }
+//   console.log(array[i])
+// }
+
+// que no imprima los pares
+// for (let i = 0; i < array.length; i++){
+//   if(array[i] % 2 === 0) {
+//     continue
+//   }
+//   console.log(array[i])
+// }
+
+// que no imprima la posición 4
+// for (let i = 0; i < array.length; i++){
+//   if(i === 4) {
+//     continue
+//   }
+//   console.log(array[i])
+// }
+
+// Break
+for(let i = 0; i < array.length; i++) {
+  if( i === 4) {
+    break
+  }
+  console.log(array[i]);
+}
+
+console.log(i);
+
+
+let array2 = [0, 1, 2, 3, 4, 5, 6];
 
 let i;
 
-for(i = 0; i < array.length; i++) {
+for(i = 0; i < array2.length; i++) {
     if( i === 4) {
       break
     }
-    console.log(array[i]);
+    console.log(array2[i]);
   }
   
-  console.log({i});
+  console.log({i}); 
