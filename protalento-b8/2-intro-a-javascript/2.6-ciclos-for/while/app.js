43d6935dd4e35a38f74ae09e40bbfc028c03706b
Yaxche <yaxchemanrique@gmail.com>
2023-06-15 18:06:26 -0700

    feat: Codigo en clase de ciclo while

---
+++ protalento-b8/2-intro-a-javascript/2.6-ciclos-for/while/app.js
@@ -0,0 +1,34 @@
+// let dato = prompt('Ingresa cualquier dato y para terminar "esc"');
+// debugger
+
+// while(dato != 'esc') {
+//     console.log(`tu dato ingresado fue ${dato}`);
+//     dato = prompt('Ingresa cualquier dato y para terminar "esc" -2');
+// }
+
+/**
+ * 1 -> console.log(1)
+ * 2 -> console.log(2)
+ * 3
+ * 4
+ * ...
+ * 10
+ */
+
+
+// let iterador = 1;
+// debugger
+// while( iterador <= 10) {
+//     console.log(iterador);
+//     iterador = iterador + 1;
+//     // iterador += 1
+//     // iterador ++
+// }
+
+let iterador = 1;
+
+while (iterador <= 10) {
+    console.log(`${iterador} x 12 = ${iterador * 12}`);
+    iterador++
+    // iterador = iterador + 1
+}
