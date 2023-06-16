db67acba45d6737d6a4223ae2c13885c77b6ceaa
Yaxche <yaxchemanrique@gmail.com>
2023-06-15 19:27:53 -0700

    feat: codigo clase - ciclos for

---
+++ protalento-b8/2-intro-a-javascript/2.6-ciclos-for/for/app.js
@@ -0,0 +1,12 @@
+let array = [0, 1, 2, 3, 4, 5, 6];
+
+let i;
+
+for(i = 0; i < array.length; i++) {
+    if( i === 4) {
+      break
+    }
+    console.log(array[i]);
+  }
+  
+  console.log({i});
