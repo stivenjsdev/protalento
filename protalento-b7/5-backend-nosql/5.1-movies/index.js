a1a12cc864580a38ae833238ce5ac574106db9b5
stiven <strujillodev@gmail.com>
2023-08-23 22:01:23 -0500

    feat: mongoose class, example

---
+++ protalento-b7/5-backend-nosql/5.1-movies/index.js
@@ -0,0 +1,50 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const server = express();
+const PORT = 3000;
+
+server.use(express.json());
+
+const main = async () => {
+    try {
+
+        // Conexiona a MongoDB
+        await mongoose.connect('mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/prueba');
+
+        // Schema
+        const clienteZapatosSchema = new mongoose.Schema({
+            name: {
+                type: String,
+                required: true,
+            },
+            lastname: {
+                type: String
+            }
+        });
+        // Model
+        const ClienteZapatos = mongoose.model('ClienteZapato', clienteZapatosSchema);
+
+        // Rutas
+        server.get('/clientes', async (req, res) => {
+            try {
+                const clientes = await ClienteZapatos.find();
+                console.log(clientes);
+                res.status(200).json(clientes);
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ message: error.message });
+            }
+        })
+
+        server.listen(PORT, () => {
+            console.log(`Servidor iniciado en http://localhost:${PORT}`);
+        });
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main()
+
