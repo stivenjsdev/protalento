80dc4dd23e3497cbd871ccdfc5a3baeb071f4a6e
stiven <strujillodev@gmail.com>
2023-09-01 01:48:43 -0500

    feat: format document

--- protalento-b7/5-backend-nosql/5.3-ref-populate/index.js
+++ protalento-b7/5-backend-nosql/5.3-ref-populate/index.js
@@ -5,136 +5,150 @@ import mongoose from 'mongoose';
 const server = express();
 const PORT = 3000;
 
-// funciona para que tome los datos JSON de una solicitud, los transforme en un objeto JavaScript 
+// funciona para que tome los datos JSON de una solicitud, los transforme en un objeto JavaScript
 // y luego los adjunte a la propiedad body del objeto request antes de llamar al controlador de ruta.
 server.use(express.json());
 server.use(cors());
 
 async function main() {
-    try {
-        // conectarnos a la bd
-        await mongoose.connect('mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/social');
+  try {
+    // conectarnos a la bd
+    await mongoose.connect(
+      'mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/social'
+    );
 
-        // USER SCHEMA
-        const userSchema = new mongoose.Schema({
-            name: String,
-            email: String,
-            // likedPosts arreglo de Posts type ObjectID
+    // USER SCHEMA
+    const userSchema = new mongoose.Schema({
+      name: String,
+      email: String,
+      // likedPosts arreglo de Posts type ObjectID
+    });
+
+    // USER MODEL
+    const User = mongoose.model('User', userSchema);
+
+    // USER ROUTES
+    server.get('/api/users', async (req, res) => {
+      try {
+        const users = await User.find();
+        console.log(users);
+
+        res.status(200).json(users);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
+
+    server.post('/api/users', async (req, res) => {
+      try {
+        if (!req.body.name) {
+          return response.status(400).json({
+            error: 'name missing',
+          });
+        }
+        const newUser = {
+          name: req.body.name,
+          email: req.body.email,
+        };
+        const user = new User(newUser);
+        const savedUser = await user.save();
+
+        res.status(201).json(savedUser);
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+      }
+    });
+
+    server.patch('/api/users/:id', async (req, res) => {
+      const userId = req.params.id;
+      const userFields = req.body;
+      try {
+        // buscar el usuario por el id y si lo encuentra lo va a actualizar con los campos que le pasemos
+        const updatedUser = await User.findByIdAndUpdate(userId, userFields, {
+          new: true,
         });
 
-        // USER MODEL
-        const User = mongoose.model("User", userSchema);
+        if (!updatedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
 
-        // USER ROUTES
-        server.get("/api/users", async (req, res) => {
-            try {
-                const users = await User.find();
-                console.log(users);
-    
-                res.status(200).json(users);
-            } catch (error) {
-                res.status(500).json({ message: error.message })
-            }
-        });
+        res.status(200).json(updatedUser);
+        // res.status(200).json({ message: 'User Updated correctly'});
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+      }
+    });
 
-        server.post("/api/users", async (req, res) => {
-            try {
-                const newUser = req.body;
-                const user = new User(newUser);
-                const savedUser = await user.save();
+    server.delete('/api/users/:id', async (req, res) => {
+      const userId = req.params.id;
+      try {
+        const deletedUser = await User.findByIdAndRemove(userId);
 
-                res.status(201).json(savedUser);
-            } catch (error) {
-                console.log(error.message);
-                res.status(500).json({ message: error.message });
-            }
-        });
+        if (!deletedUser) {
+          return res.status(404).json({ message: 'User not found' });
+        }
 
-        server.patch('/api/users/:id', async (req, res) => {
-            const userId = req.params.id;
-            const userFields = req.body;
-            try {
-                // buscar el usuario por el id y si lo encuentra lo va a actualizar con los campos que le pasemos
-                const updatedUser = await User.findByIdAndUpdate(userId, userFields, { new: true });
+        res.status(200).json(deletedUser);
+      } catch (error) {
+        console.log(error.message);
+        if (error.message.includes('Cast to ObjectId failed for value')) {
+          return res
+            .status(400)
+            .json({ message: 'Eso que enviaste no es un id' });
+        }
+        res.status(500).json({ message: error.message });
+      }
+    });
 
-                if (!updatedUser) {
-                    return res.status(404).json({ message: 'User not found'});
-                }
+    // POST SCHEMA
+    const postSchema = new mongoose.Schema({
+      title: String,
+      content: String,
+      auth: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    }); // { versionKey: false }
 
-                res.status(200).json(updatedUser);
-                // res.status(200).json({ message: 'User Updated correctly'});
-            } catch (error) {
-                console.log(error.message);
-                res.status(500).json({ message: error.message });
-            }
-        });
+    // POST MODEL
+    const Post = mongoose.model('Post', postSchema);
 
-        server.delete('/api/users/:id', async (req, res) => {
-            const userId = req.params.id;
-            try {
-                const deletedUser = await User.findByIdAndRemove(userId);
+    // POST ROUTES
+    server.get('/api/posts', async (req, res) => {
+      try {
+        const posts = await Post.find().populate('auth');
+        console.log(posts);
 
-                if (!deletedUser) {
-                    return res.status(404).json({ message: "User not found" });
-                }
+        res.status(200).json(posts);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
 
-                res.status(200).json(deletedUser);
-            } catch (error) {
-                console.log(error.message);
-                if (error.message.includes('Cast to ObjectId failed for value')) {
-                    return res.status(400).json({ message: 'Eso que enviaste no es un id' });
-                }
-                res.status(500).json({ message: error.message });
-            }
-        });
+    server.post('/api/posts', async (req, res) => {
+      try {
+        const newPost = req.body;
+        const post = new Post(newPost);
+        const savedPost = await post.save();
 
-        // POST SCHEMA
-        const postSchema = new mongoose.Schema({
-            title: String,
-            content: String,
-            auth: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "User"
-            }
-        }); // { versionKey: false }
+        res.status(201).json(savedPost);
+      } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+      }
+    });
 
-        // POST MODEL
-        const Post = mongoose.model('Post', postSchema);
+    // update and post...
 
-        // POST ROUTES
-        server.get('/api/posts', async (req, res) => {
-            try {
-                const posts = await Post.find().populate('auth');
-                console.log(posts);
-    
-                res.status(200).json(posts);
-            } catch (error) {
-                res.status(500).json({ message: error.message })
-            }
-        });
-
-        server.post("/api/posts", async (req, res) => {
-            try {
-                const newPost = req.body;
-                const post = new Post(newPost);
-                const savedPost = await post.save();
-
-                res.status(201).json(savedPost);
-            } catch (error) {
-                console.log(error.message);
-                res.status(500).json({ message: error.message });
-            }
-        });
-
-        // update and post...
-    
-        // levantar el servidor
-        server.listen(PORT, () => {
-            console.log(`Server Up in http://localhost:${PORT}`);
-        });
-    } catch (error) {
-        console.error('Error: ', error.message);
-    }
+    // levantar el servidor
+    server.listen(PORT, () => {
+      console.log(`Server up in http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error: ', error.message);
+  }
 }
 
 main();
