58126cb1756f3348cc3d3d317fdc424ec0472d71
stiven <strujillodev@gmail.com>
2023-09-05 20:55:37 -0500

    feat: create URL var

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
@@ -4,6 +4,8 @@ import mongoose from "mongoose";
 
 const server = express();
 const PORT = 3000;
+const URL =
+  "mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/social?retryWrites=true&w=majority";
 
 server.use(
   express.json()
@@ -13,19 +15,20 @@ server.use(
 ); /* me permite recibir solicitudes de clientes fuera de mi dominio */
 
 async function main() {
-  await mongoose.connect(
-    "mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/social?retryWrites=true&w=majority"
-  );
+  await mongoose.connect(URL);
 
   // USER SCHEMA
   const userSchema = new mongoose.Schema({
     name: String, // String is shorthand for {type: String}
     email: { type: String, require: true },
-    password: { type: String, require: true }
+    password: { type: String, require: true },
   });
 
   // USER MODEL
-  const User = mongoose.model('User', userSchema); /* El nombre debe ser siempre en singular */
+  const User = mongoose.model(
+    "User",
+    userSchema
+  ); /* El nombre debe ser siempre en singular */
 
   server.listen(PORT, () => {
     console.log(`Server listen in http://localhost:${PORT}`);
