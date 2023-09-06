07863e1621d067fbf3281f2672ae6925221c7cbd
stiven <strujillodev@gmail.com>
2023-09-05 20:21:14 -0500

    feat: create user schema and model

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
@@ -13,7 +13,19 @@ server.use(
 ); /* me permite recibir solicitudes de clientes fuera de mi dominio */
 
 async function main() {
-  await mongoose.connect("mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/");
+  await mongoose.connect(
+    "mongodb+srv://sensei:1234@cluster0.vep1mp4.mongodb.net/social?retryWrites=true&w=majority"
+  );
+
+  // USER SCHEMA
+  const userSchema = new mongoose.Schema({
+    name: String, // String is shorthand for {type: String}
+    email: { type: String, require: true },
+    password: { type: String, require: true }
+  });
+
+  // USER MODEL
+  const User = mongoose.model('User', userSchema); /* El nombre debe ser siempre en singular */
 
   server.listen(PORT, () => {
     console.log(`Server listen in http://localhost:${PORT}`);
@@ -21,4 +33,3 @@ async function main() {
 }
 
 main();
-
