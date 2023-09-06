3069358e30dba6f916e46d6f31e418208d74e956
stiven <strujillodev@gmail.com>
2023-09-05 19:38:04 -0500

    feat: add connection

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
@@ -1,14 +1,21 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const server = express();
 const PORT = 3000;
 
-server.use(express.json()); /* transformar el cuerpo de la peticion en un json */
-server.use(cors()); /* me permite recibir solicitudes de clientes fuera de mi dominio */
+server.use(
+  express.json()
+); /* transformar el cuerpo de la peticion en un json */
+server.use(
+  cors()
+); /* me permite recibir solicitudes de clientes fuera de mi dominio */
+
+async function main() {
+  await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.vep1mp4.mongodb.net/");
+}
 
 server.listen(PORT, () => {
-    console.log(`Server run in http://localhost:${PORT}`);
+  console.log(`Server listen in http://localhost:${PORT}`);
 });
-
-
