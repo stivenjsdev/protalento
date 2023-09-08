648357e96bdee67399e7a90bc41f16ccd4c0ecfd
stiven <strujillodev@gmail.com>
2023-09-07 19:54:03 -0500

    fix: change require to required

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/models/user.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/models/user.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 // USER SCHEMA
 const userSchema = new mongoose.Schema({
   name: String, // String is shorthand for {type: String}
-  email: { type: String, require: true },
-  password: { type: String, require: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 // USER MODEL
