e4146a0dd2d5663c01135d405621f5199154925e
stiven <strujillodev@gmail.com>
2023-09-07 20:19:11 -0500

    feat: add update route

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/routes/user.routes.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/routes/user.routes.js
@@ -26,21 +26,43 @@ userRouter.get("/:id", async (request, response) => {
   response.status(200).json(userFound);
 });
 
+// create user
 /* POST http://localhost:3000/api/v1/users */
 userRouter.post("/", async (request, response) => {
   try {
     const userProps = request.body;
-    const newUser = new User(userProps);
-    await newUser.save();
-    // or
-  
-    // await User.create({ size: "small" });
-  
+
+    // MANERA 1
+    // const newUser = new User(userProps);
+    // await newUser.save();
+    // OR...
+
+    // MANERA 2, elijan a su gusto.
+    await User.create(userProps);
+
     // response.status(201).json(newUser);
     response.status(201).end();
   } catch (error) {
     console.log(error.name);
     console.log(error.message);
-    response.status(400).json({ message: error.message });
+    response.status(400).json({ error: error.message });
+  }
+});
+
+// update user
+/* PATCH http://localhost:3000/api/v1/users */
+/* put va a reemplazar todo el recurso, y patch va a actualizar el recurso */
+userRouter.patch("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const userNewProps = request.body;
+    const updatedUser = await User.findByIdAndUpdate(id, userNewProps, { new: true }).exec();
+
+    // response.status(200).json(updatedUser);
+    response.status(200).end();
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    response.status(400).json({ error: error.message });
   }
 });
