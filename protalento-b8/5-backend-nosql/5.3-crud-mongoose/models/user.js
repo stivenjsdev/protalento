37d380570fffdcb21ab2668486161d7c0c884aad
stiven <strujillodev@gmail.com>
2023-09-07 19:11:49 -0500

    feat: separate models

---
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/models/user.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+// USER SCHEMA
+const userSchema = new mongoose.Schema({
+  name: String, // String is shorthand for {type: String}
+  email: { type: String, require: true },
+  password: { type: String, require: true },
+});
+
+// USER MODEL
+const User = mongoose.model(
+  "User",
+  userSchema
+); /* El nombre debe ser siempre en singular */
