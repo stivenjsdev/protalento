aedca87bc8df68d41efb2b478c4a26bf0f9e937b
stiven <strujillodev@gmail.com>
2023-09-07 19:54:11 -0500

    feat: add create route

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/routes/user.routes.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/routes/user.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { User } from '../models/user.js'
+import { User } from "../models/user.js";
 export const userRouter = Router();
 
 // ROUTES
 // CRUD
 // get all users
-/* http://localhost:3000/api/v1/users */
+/* GET http://localhost:3000/api/v1/users */
 userRouter.get("/", async (request, response) => {
   // find all documents
   const users = await User.find({});
@@ -14,7 +14,7 @@ userRouter.get("/", async (request, response) => {
 });
 
 // get user by id
-/* http://localhost:3000/api/v1/users/:id */
+/* GET http://localhost:3000/api/v1/users/:id */
 userRouter.get("/:id", async (request, response) => {
   const id = request.params.id;
   const userFound = await User.findById(id);
@@ -24,4 +24,23 @@ userRouter.get("/:id", async (request, response) => {
   }
 
   response.status(200).json(userFound);
+});
+
+/* POST http://localhost:3000/api/v1/users */
+userRouter.post("/", async (request, response) => {
+  try {
+    const userProps = request.body;
+    const newUser = new User(userProps);
+    await newUser.save();
+    // or
+  
+    // await User.create({ size: "small" });
+  
+    // response.status(201).json(newUser);
+    response.status(201).end();
+  } catch (error) {
+    console.log(error.name);
+    console.log(error.message);
+    response.status(400).json({ message: error.message });
+  }
 });
