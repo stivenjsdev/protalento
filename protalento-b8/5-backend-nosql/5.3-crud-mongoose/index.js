f5625db11849fd079432d62bd2db9269c52ab135
stiven <strujillodev@gmail.com>
2023-09-07 21:26:21 -0500

    feat: add logger middleware

--- protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
+++ protalento-b8/5-backend-nosql/5.3-crud-mongoose/index.js
@@ -6,10 +6,21 @@ import { userRouter } from "./routes/user.routes.js";
 const server = express();
 const PORT = 3000;
 
+// MIDDLEWARE
+function logger(request, response, next) {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('----------------------');
+  next();
+}
+
 /* transformar el cuerpo de la peticion en un json */
 server.use(express.json());
 /* me permite recibir solicitudes de clientes fuera de mi dominio */
 server.use(cors());
+
+server.use(logger);
 
 server.use("/api/v1/users", userRouter);
 
