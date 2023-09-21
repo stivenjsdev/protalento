8718f9cfe8863b65618a5d0e2b748f876e6f2535
stiven <strujillodev@gmail.com>
2023-09-20 20:05:14 -0500

    feat: intro react class

---
+++ protalento-b7/6-frontend/6.2-vite-react/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
